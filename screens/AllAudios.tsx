6816b97efc1f0b912b6597cd903cac917a8bd2ac
Jared Henderson <jared@netrivet.com>
2020-12-11 16:57:18 -0500

    show badge for new audios

--- screens/AllAudios.tsx
+++ screens/AllAudios.tsx
@@ -7,6 +7,8 @@ import { StackParamList, AudioResource } from '../types';
 import AudioListItem from '../components/AudioListItem';
 import { useSelector } from '../state';
 import * as select from '../state/selectors';
+import { Audio } from '@friends-library/friends';
+import { LANG } from '../env';
 
 interface Props {
   navigation: StackNavigationProp<StackParamList, 'Audiobooks'>;
@@ -17,21 +19,32 @@ const AllAudio: React.FC<Props> = ({ navigation }) => {
   const audios = useSelector((state) =>
     Object.values(state.audioResources)
       .filter(isDefined)
-      .map((audio) => ({
-        ...audio,
-        progress: select.progress(audio.id, state),
-      })),
+      .map((audio) => {
+        const progress = select.progress(audio.id, state);
+        return {
+          ...audio,
+          duration: Audio.humanDuration(
+            audio.parts.map((p) => p.duration),
+            `abbrev`,
+            LANG,
+          ),
+          progress,
+          isNew: isNew(audio, progress),
+        };
+      }),
   );
 
   const renderItem: (props: {
-    item: AudioResource & { progress: number };
+    item: AudioResource & { isNew: boolean; progress: number; duration: string };
   }) => JSX.Element = ({ item }) => (
     <TouchableOpacity onPress={() => navigation.navigate(`Listen`, { audioId: item.id })}>
       <AudioListItem
         id={item.id}
         title={item.title}
         friend={item.friend}
+        duration={item.duration}
         progress={item.progress}
+        isNew={item.isNew}
       />
     </TouchableOpacity>
   );
@@ -42,3 +55,13 @@ const AllAudio: React.FC<Props> = ({ navigation }) => {
 };
 
 export default AllAudio;
+
+function isNew(audio: AudioResource, progress: number): boolean {
+  if (progress > 4) {
+    return false;
+  }
+  return Date.now() - Number(new Date(audio.date)) < NEW_MS;
+}
+
+// 60 days
+const NEW_MS = 1000 * 60 * 60 * 24 * 60;
