c6397dd4d25768622a50937e46391d12283aa6d9
Jared Henderson <jared@netrivet.com>
2020-12-15 17:08:54 -0500

    sort audios

--- components/ListControls.tsx
+++ components/ListControls.tsx
@@ -1,26 +1,87 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
+import { BLUE_HEX } from '@friends-library/theme';
 import { PropSelector, useDispatch, useSelector } from '../state';
-import { setSearchQuery } from '../state/preferences';
+import { setSearchQuery, setSortAudiosBy, AudioSortCriteria } from '../state/preferences';
 import tw from '../lib/tailwind';
+import { Sans } from './Text';
 import Search from './Search';
 
 interface Props {
   query: string;
   setQuery: (query: string) => unknown;
+  sort: AudioSortCriteria;
+  setSort: (criteria: AudioSortCriteria) => unknown;
 }
 
-export const ListControls: React.FC<Props> = ({ query, setQuery }) => (
+export const ListControls: React.FC<Props> = ({ query, setQuery, sort, setSort }) => (
   <View style={tw(`p-2 pt-4`)}>
     <Search query={query} setQuery={setQuery} />
+    <View
+      style={tw(
+        `flex-row justify-center mt-4 -mb-1 pb-2 flex-wrap border-b border-gray-300`,
+      )}
+    >
+      <SortButton
+        active={sort === `published`}
+        onPress={() => setSort(`published`)}
+        label="Newest"
+      />
+      <SortButton
+        active={sort === `length_reverse`}
+        onPress={() => setSort(`length_reverse`)}
+        label="Short"
+      />
+      <SortButton
+        active={sort === `length`}
+        onPress={() => setSort(`length`)}
+        label="Long"
+      />
+      <SortButton
+        active={sort === `alphabetical`}
+        onPress={() => setSort(`alphabetical`)}
+        label="Title"
+        last
+      />
+    </View>
   </View>
+);
+
+const SortButton: React.FC<{
+  label: string;
+  onPress: () => unknown;
+  active: boolean;
+  last?: boolean;
+}> = ({ label, last, active, onPress }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={tw(
+      `py-1 px-4 flex-grow justify-center rounded-md mb-2`,
+      !last && `mr-2`,
+      { maxWidth: 150 },
+      !active && { backgroundColor: `#ddd` },
+      active && { backgroundColor: BLUE_HEX },
+    )}
+  >
+    <Sans
+      style={tw(`text-center uppercase`, {
+        'text-gray-700': !active,
+        'text-white font-bold': active,
+      })}
+      size={10}
+    >
+      {label}
+    </Sans>
+  </TouchableOpacity>
 );
 
 export const propSelector: PropSelector<OwnProps, Props> = (ownProps, dispatch) => {
   return (state) => {
     return {
       query: state.preferences.searchQuery,
+      sort: state.preferences.sortAudiosBy,
       setQuery: (query) => dispatch(setSearchQuery(query)),
+      setSort: (criteria) => dispatch(setSortAudiosBy(criteria)),
     };
   };
 };
