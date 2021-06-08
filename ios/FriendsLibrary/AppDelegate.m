81d104b2b50f1c3998b81e3eea03561f653994ec
Jared Henderson <jared@netrivet.com>
2021-06-08 15:39:08 -0400

    fix import of header, move outside of #ifdef

--- ios/FriendsLibrary/AppDelegate.m
+++ ios/FriendsLibrary/AppDelegate.m
@@ -5,6 +5,9 @@
 #import <React/RCTRootView.h>
 #import "RNSplashScreen.h"
 
+// react-native-home-indicator
+#import <RNHomeIndicator.h>
+
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -12,9 +15,6 @@
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
 #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-
-// react-native-home-indicator
-#import <RNHomeIndicator.h>
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
