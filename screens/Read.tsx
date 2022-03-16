8bdc40d02bc0ab54b53fd8987a3a5ccdc0b886db
Jared Henderson <jaredh159@users.noreply.github.com>
2022-03-16 14:22:34 -0400

    separate Read screen into container/non-container files

--- screens/Read.tsx
+++ screens/Read.tsx
@@ -1,25 +1,18 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { PureComponent, useEffect, useState } from 'react';
+import React, { PureComponent } from 'react';
 import { View, StatusBar, GestureResponderEvent, Platform } from 'react-native';
 import { WebView, WebViewMessageEvent } from 'react-native-webview';
 import Popover, { PopoverMode } from 'react-native-popover-view';
 import { AnyAction } from 'redux';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { RouteProp } from '@react-navigation/native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import * as T from '../types';
 import FullscreenLoading from '../components/FullscreenLoading';
 import EbookError from '../components/EbookError';
 import tw from '../lib/tailwind';
-import Editions from '../lib/Editions';
-import { useSelector, PropSelector, useDispatch, Dispatch } from '../state';
-import { readScreenProps } from './read-helpers';
+import { Dispatch } from '../state';
 import { wrapHtml, Message } from '../lib/ebook-code';
 import * as gesture from '../lib/gesture';
 import { setEbookPosition } from '../state/ebook/position';
 import EbookSettings from '../components/EbookSettings';
 import { toggleShowingEbookHeader, toggleShowingEbookSettings } from '../state/ephemeral';
-import { setLastEbookEditionId } from '../state/resume';
 import ReadFooter from '../components/ReadFooter';
 
 // @ts-ignore
@@ -61,7 +54,7 @@ interface State {
   position?: number;
 }
 
-class Read extends PureComponent<Props, State> {
+export default class Read extends PureComponent<Props, State> {
   private htmlRef: React.MutableRefObject<string | null>;
   private intervalRef: React.MutableRefObject<ReturnType<typeof setTimeout> | null>;
   private webViewRef: React.RefObject<WebView>;
@@ -303,120 +296,6 @@ class Read extends PureComponent<Props, State> {
     );
   }
 }
-
-/**
- * The `html` and `css` props (for rendering the ebook)
- * we have to get _asynchronously_ from the filesystem or network.
- * This type models the rest of the props we can get immediately from state.
- */
-export interface SyncProps {
-  resource: T.EditionResource;
-  networkConnected: boolean;
-  position: number;
-  fontSize: number;
-  justify: boolean;
-  colorScheme: T.EbookColorScheme;
-  showingSettings: boolean;
-  showingHeader: boolean;
-  headerHeight: number;
-}
-
-interface OwnProps {
-  navigation: StackNavigationProp<T.StackParamList, 'Read'>;
-  route: RouteProp<T.StackParamList, 'Read'>;
-}
-
-const propSelector: PropSelector<OwnProps, SyncProps> = (ownProps) => (state) => {
-  const editionId = ownProps.route.params.editionId;
-  const resource = Editions.get(editionId);
-  if (!resource) return null;
-  return {
-    resource,
-    colorScheme: state.preferences.ebookColorScheme,
-    fontSize: state.preferences.ebookFontSize,
-    justify: state.preferences.ebookJustify,
-    position: state.ebook.position[editionId] || 0,
-    networkConnected: state.network.connected,
-    showingSettings: state.ephemeral.showingEbookSettings,
-    showingHeader: state.ephemeral.showingEbookHeader,
-    headerHeight: state.dimensions.ebookHeaderHeight,
-  };
-};
-
-type ContainerState =
-  | { state: `loading` }
-  | { state: `error`; reason: 'no_internet' | 'unknown' }
-  | { state: `ready`; html: string; css: string; initialPosition: number };
-
-const ReadContainer: React.FC<OwnProps> = (ownProps) => {
-  const insets = useSafeAreaInsets();
-  const dispatch = useDispatch();
-  const props = useSelector(propSelector(ownProps, dispatch));
-  const [containerState, setContainerState] = useState<ContainerState>({
-    state: `loading`,
-  });
-
-  useEffect(() => {
-    dispatch(setLastEbookEditionId(ownProps.route.params.editionId));
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (!props) {
-      setContainerState({ state: `error`, reason: `unknown` });
-      return;
-    }
-
-    const shouldNetworkRetry =
-      containerState.state === `error` &&
-      containerState.reason === `no_internet` &&
-      props.networkConnected === true;
-
-    if (containerState.state === `loading` || shouldNetworkRetry) {
-      (async () => {
-        const result = await readScreenProps(props.resource, props.networkConnected);
-        if (result.success) {
-          setContainerState({
-            state: `ready`,
-            initialPosition: props.position,
-            ...result.value,
-          });
-        } else {
-          setContainerState({ state: `error`, reason: result.error });
-        }
-      })();
-    }
-  }, [
-    containerState.state,
-    containerState.state === `error` ? containerState.reason : null,
-    props?.networkConnected,
-    setContainerState,
-    props?.resource,
-  ]);
-
-  if (containerState.state !== `ready`) {
-    return <Read {...containerState} colorScheme={props?.colorScheme || `white`} />;
-  }
-
-  return (
-    <Read
-      {...containerState}
-      editionId={props!.resource.id}
-      position={containerState.initialPosition}
-      colorScheme={props!.colorScheme}
-      fontSize={props!.fontSize}
-      justify={props!.justify}
-      showingSettings={props!.showingSettings}
-      showingHeader={props!.showingHeader}
-      headerHeight={props!.headerHeight}
-      dispatch={dispatch}
-      safeAreaTopOffset={insets.top}
-      safeAreaBottomOffset={insets.bottom}
-      chapterId={ownProps.route.params.chapterId}
-    />
-  );
-};
-
-export default ReadContainer;
 
 /**
  * Because of ios scroll physics/bounce things, you can get negative
